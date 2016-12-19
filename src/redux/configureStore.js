b066dc234d5e7b43241c990e1e22244c0c475ebf
Jakub Kottnauer <jk@jakubkottnauer.com>
2016-12-19 08:17:54 +0100

    Add real middlewares

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,61 +1,25 @@
+import createLogger from 'redux-logger';
 import myReducer from './reducer';
-import { combineReducers, createStore } from 'redux';
-
-// STEP 2 - better but not quite there
-// function myLogger(store) {
-//   const next = store.dispatch;
-//   return function myDispatch(action) {
-//     console.log('dispatching...', action.type, action.payload);
-//     const result = next(action);
-//     console.log('dispatched', result);
-//     return result;
-//   }
-// }
-
-// function applyMiddleware(store, middlewares = []) {
-//   middlewares.forEach(m => store.dispatch = m(store))
-// }
-
-
-
-// STEP 3 - yes!
-// const myLogger = store => next => action => {
-//   console.log('dispatching...', action.type, action.payload);
-//   const result = next(action);
-//   console.log('dispatched', result);
-//   return result;
-// }
-
-// function applyMiddleware(store, middlewares = []) {
-//   let { dispatch } = store;
-//   middlewares.forEach(m => dispatch = m(store)(dispatch))
-//   return { ...store, dispatch };
-// }
+import promiseMiddleware from 'redux-promise-middleware';
+import thunk from 'redux-thunk';
+import { applyMiddleware, combineReducers, createStore } from 'redux';
 
 export default function configureStore() {
   const reducers = combineReducers({
     myReducer
   });
 
-  const store = createStore(reducers);
+  const middlewares = [
+    thunk,
+    promiseMiddleware({
+      promiseTypeSuffixes: ['START', 'SUCCESS', 'ERROR']
+    }),
+    createLogger({
+      collapsed: true
+    })
+  ];
 
-  // STEP 1 - monkey-patching ugly!
-  // const next = store.dispatch;
-  // store.dispatch = function myDispatch(action) {
-  //   console.log('dispatching...', action.type, action.payload);
-  //   const result = next(action);
-  //   console.log('dispatched', result);
-  //   return result;
-  // }
-
-
-  // STEP 2
-  // applyMiddleware(store, [myLogger])
-
-  // STEP 3
-  // const improvedStore = applyMiddleware(store, [myLogger])
-
-  // STEP 4 - use applyMiddleware from redux
+  const store = createStore(reducers, applyMiddleware(...middlewares));
 
   return store;
 }
